73247d1674d5308a723eecc2e7e65c2b7f7777e6
ZK <dev_zk@163.com>
2016-12-02 13:47:19 +0800

    sync

--- ht-wechat-dev/js/base.js
+++ ht-wechat-dev/js/base.js
@@ -1,4 +1,19 @@
 /**
  * Created by ZK on 16/12/1.
  */
-var baseUrl = 'http://119.29.77.36:8001/api/'; //接口IP地址和端口
+
+//接口IP地址和端口
+var baseUrl = 'http://119.29.77.36:8001/api/';
+
+//建立一個可存取到該file的url
+function getObjectURL(file) {
+    var url = null ;
+    if (window.createObjectURL!=undefined) { // basic
+        url = window.createObjectURL(file) ;
+    } else if (window.URL!=undefined) { // mozilla(firefox)
+        url = window.URL.createObjectURL(file) ;
+    } else if (window.webkitURL!=undefined) { // webkit or chrome
+        url = window.webkitURL.createObjectURL(file) ;
+    }
+    return url ;
+}
