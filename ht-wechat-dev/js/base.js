b7fde8ac973ce4721ba78564a161157e2d790a05
ZK <dev_zk@163.com>
2016-12-12 18:15:47 +0800

    lil

--- ht-wechat-dev/js/base.js
+++ ht-wechat-dev/js/base.js
@@ -5,6 +5,30 @@
 //接口IP地址和端口
 var baseUrl = 'http://119.29.77.36:8001/api/';
 
+var domainConfig = {};
+if (!$.cookie('downloadDomain')) {
+    $.ajax({
+        url: baseUrl + 'data/getDomainInfo',
+        type: 'POST',
+        data: {
+            platform: '2',
+        },
+        success: function (data) {
+            var download = data.p.domain.downloadDomain + '/';
+            domainConfig.downloadDomain = download;
+            $.cookie('downloadDomain', download, {
+                expires: -1,
+            });
+        },
+        error: function () {
+
+        }
+    });
+}
+else {
+    domainConfig.downloadDomain = $.cookie('downloadDomain');
+}
+
 //建立一個可存取到該file的url
 function getObjectURL(file) {
     var url = null ;
@@ -16,4 +40,26 @@ function getObjectURL(file) {
         url = window.webkitURL.createObjectURL(file) ;
     }
     return url ;
+}
+
+function getQueryStringArgs() {
+    var qs = (location.search.length > 0 ? location.search.substring(1) : ''),
+        args = {},
+        items = qs.length ? qs.split('&') : [],
+        item = null,
+        name = null,
+        value = null,
+        i = 0,
+        len = items.length;
+    for (i = 0; i < len; i ++) {
+        item = items[i].split('=');
+        name = decodeURIComponent(item[0]);
+        value = decodeURIComponent(item[1]);
+
+        if (name.length) {
+            args[name] = value;
+        }
+    }
+
+    return args;
 }
