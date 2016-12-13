a439ad34be9e8a3cc1a11cbdc7ba55147e225b57
ZK <dev_zk@163.com>
2016-12-13 10:39:03 +0800

    add loading

--- ht-wechat-dev/js/base.js
+++ ht-wechat-dev/js/base.js
@@ -5,29 +5,29 @@
 //接口IP地址和端口
 var baseUrl = 'http://119.29.77.36:8001/api/';
 
-var domainConfig = {};
-if (!$.cookie('downloadDomain')) {
-    $.ajax({
-        url: baseUrl + 'data/getDomainInfo',
-        type: 'POST',
-        data: {
-            platform: '2',
-        },
-        success: function (data) {
-            var download = data.p.domain.downloadDomain + '/';
-            domainConfig.downloadDomain = download;
-            $.cookie('downloadDomain', download, {
-                expires: -1,
-            });
-        },
-        error: function () {
-
-        }
-    });
-}
-else {
-    domainConfig.downloadDomain = $.cookie('downloadDomain');
-}
+// var domainConfig = {};
+// if (!$.cookie('downloadDomain')) {
+//     $.ajax({
+//         url: baseUrl + 'data/getDomainInfo',
+//         type: 'POST',
+//         data: {
+//             platform: '2',
+//         },
+//         success: function (data) {
+//             var download = data.p.domain.downloadDomain + '/';
+//             domainConfig.downloadDomain = download;
+//             $.cookie('downloadDomain', download, {
+//                 expires: -1,
+//             });
+//         },
+//         error: function () {
+//
+//         }
+//     });
+// }
+// else {
+//     domainConfig.downloadDomain = $.cookie('downloadDomain');
+// }
 
 //建立一個可存取到該file的url
 function getObjectURL(file) {
@@ -63,3 +63,16 @@ function getQueryStringArgs() {
 
     return args;
 }
+
+// loading图
+var loading = {
+    $loadingContainer: $('<div style="background: rgba(255, 255, 255, .95); position: fixed; width: 100%; height: 100%; top: 0; left: 0;">'),
+    $loadImg: $('<img src="imgs/loding.gif" alt="" style="position: absolute; width: 90px; height: 90px; top: 50%; left: 50%; margin-left: -45px; margin-top: -55px">'),
+    show: function () {
+        this.$loadingContainer.append(this.$loadImg);
+        $('body').append(this.$loadingContainer);
+    },
+    hide: function () {
+        this.$loadingContainer.remove();
+    }
+};
